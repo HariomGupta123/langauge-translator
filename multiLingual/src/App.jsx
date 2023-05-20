5e2519e04326ad78eec1bcc0e6ca620c0cb20eef
Hariom Gupta <hg7716505@gmail.com>
2023-05-20 17:26:46 +0545

    laungauge translation Api and translate 30 langauges to another 30 langauge

--- multiLingual/src/App.jsx
+++ multiLingual/src/App.jsx
@@ -1,13 +1,45 @@
 import { useEffect, useState } from "react";
 import "./App.css"
-import axios from "axios"
+import axios, { Axios } from "axios"
 
 const App=()=>{
   const [options,setOption]=useState([]);
-  const [to,setTo]=useState("english");
-  const [from,setFrom]=useState("hindi");
-  
+  const [to,setTo]=useState("hi");
+  const [from,setFrom]=useState("en");
+  const [input,setInput]=useState();
+    const [output, setOutput] = useState('');
+
+  const traslate= async ()=>{
+    {
+      /*curl -X POST "https://libretranslate.org/translate" -H  "accept: application/json" -H  "Content-Type: application/x-www-form-urlencoded" -d "q=hello&source=en&target=es&format=text&api_key=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+       */
+    }
+    const params=new URLSearchParams();
+    params.append('q',input);
+     params.append('source',from);
+     params.append('target', to);
+     params.append("api_key", "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx");
+    
+const respect=await  axios.post(
+        "https://libretranslate.org/translate",params,
+        {
+          q: input,
+          source: from,
+          target: to,
+          api_key: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+        },
+        {
+          headers: {
+            accept: "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+          }
+        }
+      )
       
+        console.log(respect.data)
+        setOutput(respect.data.translatedText);
+      
+  }
 
  
     
@@ -41,8 +73,12 @@ fetch();
       <h1>multilingaul</h1>
       <div className="select">
         <div>
-        from({from}):
-          <select onChange={(e)=>{setFrom(e.target.value)}}>
+          from({from}):
+          <select
+            onChange={(e) => {
+              setFrom(e.target.value);
+            }}
+          >
             {options.map((opt) => {
               return (
                 <option value={opt.code} key={opt.code}>
@@ -53,8 +89,12 @@ fetch();
           </select>
         </div>
         <div>
-        to({to}):
-          <select onChange={(e)=>{setTo(e.target.value)}}>
+          to({to}):
+          <select
+            onChange={(e) => {
+              setTo(e.target.value);
+            }}
+          >
             {options.map((opt) => {
               return (
                 <option value={opt.code} key={opt.code}>
@@ -66,8 +106,26 @@ fetch();
         </div>
       </div>
       <div>
-        <textarea rows={4} cols={40}></textarea>
-        <textarea rows={4} cols={40}></textarea>
+        <textarea
+          rows={4}
+          cols={40}
+          onInput={(e) => {
+            setInput(e.target.value);
+          }}
+        ></textarea>
+        <textarea rows={4} cols={40} value={output}>
+       
+          {output}
+        </textarea>
+      </div>
+      <div>
+        <button
+          onClick={(e) => {
+            traslate();
+          }}
+        >
+          translate
+        </button>
       </div>
     </>
   );
